c310125214631df92e57092da58a787c09cdf722
wandyirawan <wandy.irawan17@gmail.com>
2018-09-21 08:01:06 +0700

    add new feature

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Discuss.Mixfile do
   def application do
     [mod: {Discuss, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :ueberauth, :ueberauth_github]]
   end
 
   # Specifies which paths to compile per environment.
@@ -31,13 +31,15 @@ defmodule Discuss.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.2.5"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_pubsub, "~> 1.0.2"},
+     {:phoenix_ecto, "~> 3.3.0"},
+     {:postgrex, ">= 0.13.0"},
+     {:phoenix_html, "~> 2.10"},
+     {:phoenix_live_reload, "~> 1.1.5", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ueberauth, "~> 0.3"},
+     {:ueberauth_github, "~> 0.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
