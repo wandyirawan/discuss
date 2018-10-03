6c9d151d8b86934a05419a7981660d5ca4075cb2
wandyirawan <wandy.irawan17@gmail.com>
2018-10-03 07:20:59 +0700

    add new model

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,9 +1,36 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
-  plug Ueberauth
+  plug Ueberauth 
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> 
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+
   end
 end
