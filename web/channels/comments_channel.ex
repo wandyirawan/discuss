babd88435d030b2e5882a89730305dab1266a252
wandyirawan <wandy.irawan17@gmail.com>
2018-10-08 06:57:46 +0700

    add new model

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,13 +1,29 @@
 defmodule Discuss.CommentsChannel do 
   use Discuss.Web, :channel
+  alias Discuss.Topic
 
-  def join(name,_params, socket) do
-    IO.puts("==+++++==")
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id,_params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, asign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, %{content => content}, socket) do
+    topic = socket.assigns.topic
 
+    changeset = 
+      Topic
+      |> build_assoc(:contents)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+
+    {:reply, :ok, socket}
   end
 end
